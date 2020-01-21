baae3f58383b67bf03572db2c44bb0ee2eb77e65
bbmek99 <mackychinma@gmail.com>
2020-01-21 15:17:02 +0700

    fix

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.port || 3000;
-const promotion = require("./function");
+const promotion = require("./promotion");
 
 app.get("/", (req, res) => {
   res.send("good");
 });
 
 app.listen(PORT, () => {
-  //   console.log(`app is running on port ${PORT}`);
   promotion([
-    { proID: "01", amount: 5, price: 8000, total: 40000 },
-    { proID: "02", amount: 3, price: 8000, total: 24000 },
-    { proID: "03", amount: 2, price: 9000, total: 18000 }
+    { proID: "01", qty: 4, price: 8000, discount: 0, promotionID: "" },
+    { proID: "02", qty: 3, price: 8000, discount: 0, promotionID: "" },
+    { proID: "05", qty: 12, price: 9000, discount: 0, promotionID: "" }
   ]);
 });
---
+++ promotion.js
@@ -0,0 +1,100 @@
+const _ = require("lodash");
+module.exports = products => {
+  // seu laiy un loud la kha
+  function checkPro1() {
+    promotion = {
+      proName: "discount1",
+      proID: "05",
+      qty: 5,
+      discountPrice: 5000
+    };
+    for (let i = 0; i < products.length; i++) {
+      if (_.includes(promotion.proID, products[i].proID)) {
+        let qty = products[i].qty;
+        if (qty >= promotion.qty) {
+          products[i].promotionID = promotion.proName;
+          let unit = parseInt(products[i].qty / promotion.qty);
+          products[i].discount = promotion.discountPrice * unit;
+        }
+      }
+    }
+    return products;
+  }
+  function checkPro2() {
+    promotion = {
+      proName: "discount",
+      products: [
+        { proID: "01", qty: 2, discount: 1000 },
+        { proID: "02", qty: 5, discount: 1000 }
+      ]
+    };
+    for (let i = 0; i < products.length; i++) {
+      let matchIndex = _.findIndex(promotion.products, product => {
+        return (
+          product.proID == products[i].proID && product.qty <= products[i].qty
+        );
+      });
+
+      if (matchIndex > -1) {
+        let matchID = promotion.products[matchIndex].proID;
+
+        let matchIdIndex = _.findIndex(products, ["proID", matchID]);
+        console.log(matchIdIndex);
+        products[matchIdIndex].price =
+          products[matchIdIndex].price -
+          promotion.products[matchIndex].discount;
+      }
+    }
+
+    return products;
+  }
+  // 2 un 15.000
+  // la kha khong koum sin kha tong thao kun
+  function checkPro3() {
+    promotion = {
+      proName: "discount3",
+      proID: ["01", "02", "03", "04"],
+      discountPrice: 1000
+    };
+    let qty = 0;
+    for (let i = 0; i < products.length; i++) {
+      if (_.includes(promotion.proID, products[i].proID)) {
+        let amount = products[i].qty + qty;
+        if (amount >= 2) {
+            console.log(products[i].qty)
+          qty = amount % 2;
+          products[i].discount = promotion.discountPrice * parseInt(amount / 2);
+          products[i].promotionID = promotion.proName;
+        }
+      }
+    }
+    console.log(`qty ${qty}`);
+    return products;
+  }
+  // theam
+  function checkPro4() {
+    promotion = {
+      proName: "discount4",
+      proID: "05",
+      qty: 5,
+      freeID: "06"
+    };
+    for (let i = 0; i < products.length; i++) {
+      if (_.includes(promotion.proID, products[i].proID)) {
+        let qty = products[i].qty;
+        if (qty >= promotion.qty) {
+          products.push({
+            proID: promotion.freeID,
+            qty: 1,
+            price: 5000,
+            discount: 5000,
+            promotionID: "discount4"
+          });
+          break;
+        }
+      }
+    }
+    return products;
+  }
+  console.log(checkPro3());
+};
